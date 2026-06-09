a0eefd4fe09051afa37e7eeb4d22511fdbdb0c36
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-09 19:39:11 -0400

    Creación de controladores 9/6

---
+++ Controllers/IndiceMasaCorporalController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IndiceMasaCorporalController : Controller
+    {
+        [HttpGet("Calcular Indice de Masa Corporal")]
+        public IActionResult CalcularIndiceMasaCorporal(double peso, double altura)
+        {
+            if (altura <= 0)
+            {
+                return BadRequest("La altura debe ser mayor que cero.");
+            }
+
+            double imc = peso / (altura * altura);
+            string categoria;
+
+            if (imc < 18.5)
+            {
+                categoria = "Bajo peso";
+            }
+            else if (imc < 24.9)
+            {
+                categoria = "Normal";
+            }
+            else if (imc < 29.9)
+            {
+                categoria = "Sobrepeso";
+            }
+            else
+            {
+                categoria = "Obesidad";
+            }
+
+            return Ok($"El IMC es {imc:F2} y la categoría es {categoria}.");
+        }
+    }
+}
---
+++ Controllers/TextoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TextoController : Controller
+    {
+        [HttpGet("Texto")]
+        public IActionResult ContarTexto(string texto)
+        {
+            int longitud = texto.Length;
+            int palabras = texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            int vocales = texto.Count(c => "aeiouAEIOU".Contains(c));
+            return Ok($"El texto tiene {longitud} caracteres, {palabras} palabras y {vocales} vocales.");
+        }
+
+        [HttpGet("InvertirTexto")]
+        public IActionResult InvertirTexto(string texto)
+        {
+            char[] caracteres = texto.ToCharArray();
+            Array.Reverse(caracteres);
+            string textoInvertido = new string(caracteres);
+            return Ok($"El texto invertido es: {textoInvertido}");
+        }
+    }
+}
