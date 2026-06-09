244fc2f2264e7be8b03f06510eccc6982a523705
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-09 19:15:18 -0400

    Create HolaController.cs

---
+++ Controllers/HolaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HolaController : Controller
+    {
+        [HttpGet("Saludar")]
+        public IActionResult Saludar(string nombre)
+        {
+            return Ok($"Hola, {nombre}!");
+        }
+    }
+}
