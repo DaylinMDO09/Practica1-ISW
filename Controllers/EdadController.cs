62b7af0483e5b12f4329b9fd01c1cc7c906d8daf
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-10 21:53:02 -0400

    creacion de propinacontroller y edadcontroller

---
+++ Controllers/EdadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EdadController : Controller
+    {
+        [HttpGet("CalcularEdad")]
+        public IActionResult CalcularEdad(DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+            if (fechaNacimiento.Date > fechaActual.AddYears(-edad)) edad--;
+
+            string signoZodiacal = ObtenerSignoZodiacal(fechaNacimiento);
+            return Ok(new { fechaNacimiento, edad, signoZodiacal });
+        }
+
+        private string ObtenerSignoZodiacal(DateTime fechaNacimiento)
+        {
+            int dia = fechaNacimiento.Day;
+            int mes = fechaNacimiento.Month;
+
+            switch (mes)
+            {
+                case 1: return (dia <= 20) ? "Capricornio" : "Acuario";
+                case 2: return (dia <= 19) ? "Acuario" : "Piscis";
+                case 3: return (dia <= 20) ? "Piscis" : "Aries";
+                case 4: return (dia <= 20) ? "Aries" : "Tauro";
+                case 5: return (dia <= 21) ? "Tauro" : "Géminis";
+                case 6: return (dia <= 21) ? "Géminis" : "Cáncer";
+                case 7: return (dia <= 22) ? "Cáncer" : "Leo";
+                case 8: return (dia <= 23) ? "Leo" : "Virgo";
+                case 9: return (dia <= 23) ? "Virgo" : "Libra";
+                case 10: return (dia <= 23) ? "Libra" : "Escorpio";
+                case 11: return (dia <= 22) ? "Escorpio" : "Sagitario";
+                case 12: return (dia <= 21) ? "Sagitario" : "Capricornio";
+                default: return "Desconocido";
+            }
+        }
+    }
+}
---
+++ Controllers/PropinaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PropinaController : Controller
+    {
+        [HttpGet("CalcularPropina")]
+        public IActionResult CalcularPropina(double monto, double porcentaje)
+        {
+            double propina = monto * porcentaje / 100;
+            double total = monto + propina;
+            return Ok(new {monto, porcentaje, propina, total});
+        }
+    }
+}
