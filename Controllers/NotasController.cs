76bc23fa0b87ae9862e666feec84f0d2befe5628
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-10 22:01:21 -0400

    Create NotasController.cs

---
+++ Controllers/NotasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotasController : Controller
+    {
+        [HttpPost("Notas")]
+        public IActionResult Notas([FromBody] NotasRequest request)
+        {
+            var notas = request.Notas;
+            double promedio = notas.Average();
+            double maximo = notas.Max();
+            double minimo = notas.Min();
+            int aprobados = notas.Count(n => n >= 70);
+            int reprobados = notas.Count(n => n < 70);
+            return Ok(new { notas, promedio, maximo, minimo, aprobados, reprobados });
+        }
+    }
+
+    public class NotasRequest
+    {
+        public List<double> Notas { get; set; }
+    }
+}
