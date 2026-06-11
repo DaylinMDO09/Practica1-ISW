939b371eec14940e34b7318ef65fc2b468249e6a
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-10 21:35:38 -0400

    Create FechasController.cs

---
+++ Controllers/FechasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1_ISW.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FechasController : Controller
+    {
+        [HttpGet("DiferenciaEntreFechas")]
+        public IActionResult DiferenciaEntreFechas(DateTime desde, DateTime hasta)
+        {
+            TimeSpan diferencia = hasta - desde;
+            return Ok($"La diferencia entre las fechas es de {diferencia.Days} días.");
+        }
+        [HttpGet("AgregarFecha")]
+        public IActionResult AgregarFecha(DateTime fecha, int dias)
+        {
+            DateTime nuevaFecha = fecha.AddDays(dias);
+            return Ok($"La nueva fecha después de agregar {dias} días es: {nuevaFecha:dd/MM/yyyy}");
+        }
+    }
+}
